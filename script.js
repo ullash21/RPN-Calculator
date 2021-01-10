8a726f0c83c443ecea87d8169acefd8a1c9e86b9
ismam <ishmamalhoque@gmail.com>
2021-01-10 20:57:33 +0800

    try touch start touch end for buttons

--- script.js
+++ script.js
@@ -178,15 +178,15 @@ function addHover(button) {
   button.addEventListener("mouseover",function(){
     button.style.background="#fed8b1"//light orange
   })
-  // button.addEventListener("touchstart",function(){
-  //   button.style.background="#fed8b1"//light orange
-  // })
+  button.addEventListener("touchstart",function(){
+    button.style.background="#fed8b1"//light orange
+  })
 }
 function delHover(button) {
   button.addEventListener("mouseout",function(){
     button.style.background="#EFEFEF"// lightgray
   })
-  // button.addEventListener("touchend",function(){
-  //   button.style.background="#EFEFEF"// lightgray
-  // })
+  button.addEventListener("touchend",function(){
+    button.style.background="#EFEFEF"// lightgray
+  })
 }
